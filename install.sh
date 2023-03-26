a8df926c0a0d25f6ac890cdf15ec535f4af0d0e3
Jack Zellweger <20098240+jackzellweger@users.noreply.github.com>
2023-03-25 22:03:24 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,25 @@
 sudo apt-get install -y sqlite3
 
 # Create directory for the SQLite database
-sudo mkdir -p /opt/rent-to-sale-price-ratios/data/db
+# sudo mkdir -p /opt/rent-to-sale-price-ratios/data/db
 
 # Install Python 3, pip, and venv
 sudo apt-get install -y python3 python3-pip python3-venv
 
 # Install Jupyter
 pip3 install jupyter
+
+# Create virtual environment
+python3 -m venv myenv
+
+# Activate virtual environment
+source myenv/bin/activate
+
+# Install required python libraries
+pip install pandas geopandas pathlib folium branca json os
+
+# Create requirements.txt file
+pip freeze > requirements.txt
 
 echo "Packages and dependencies have been installed."
 
